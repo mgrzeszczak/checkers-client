0abdc19fb8aa1463cb12728e0937677aff57b75d
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2015-12-30 18:27:04 +0100

    Fixed pom.

--- src/main/java/pl/mg/checkers/controller/MenuController.java
+++ src/main/java/pl/mg/checkers/controller/MenuController.java
@@ -32,7 +32,7 @@ public class MenuController {
             sceneService.showAlert(Alert.AlertType.ERROR,"Error","Invalid nickname","Please enter a valid nickname");
             return;
         }
-        connectionService.connect("http://www.greyshack.bbarwik.com",8080,nickField.getText());
+        connectionService.connect("greyshack.bbarwik.com",8080,nickField.getText());
     }
 
 }
